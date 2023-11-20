3bd3acbb8053d8d3dd084d9e87909e6e50859322
Damian <damianomiotek@outlook.com>
2023-11-20 18:30:18 +0100

    Refreshed NoPage
    Refreshed (Not final ?) design

--- src/public/pages/NoPage.tsx
+++ src/public/pages/NoPage.tsx
@@ -7,7 +7,7 @@ export default function NoPage(){
         <section className={styles.FlexContainer}>
             <img src="/ilustrations/ResourceNotFound.svg" alt="'404' Image" />
             <div className={styles.MessageHolder}>
-                <h2>Page not found</h2>
+                <h3>Page not found</h3>
                 <p>Resource you are looking for doesn't exist under that address.</p>
                 <Link to="/" className="button">Take me home</Link>
             </div>
