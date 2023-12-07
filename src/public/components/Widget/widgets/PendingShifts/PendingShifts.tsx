dc2b4dcf1adc41032ac722ca18927ee55143d6de
Damian <damianomiotek@outlook.com>
2023-12-07 17:38:46 +0100

    Design cleanup

--- src/public/components/Widget/widgets/PendingShifts/PendingShifts.tsx
+++ src/public/components/Widget/widgets/PendingShifts/PendingShifts.tsx
@@ -1,46 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 
 import classes from "./PendingShifts.css";
-import commonClasses from "../../../common.css";
 
 import { API } from "../../../../types/networkAPI";
 import { callAPI } from "../../../../modules/utils";
 import { Link } from "react-router-dom";
 import { WidgetContext } from "../../WidgetBox";
-
-function renderMainPanel() {
-    return (
-        <div className={classes.MainContent}>
-            <h4>3 shifts</h4>
-            <h5>You still have shifts this month that require finishing.</h5>
-            <Link className={"button"} to={"/Statistics/Shifts?state=pending"}>Go to shifts</Link>
-        </div>
-    );
-}
-
-function renderDummyContent() {
-    const result = [];
-
-    for(let i=0; i < 3; i++) {
-        result.push(
-            <li key={i}>
-                <h5 className={commonClasses.PulseLoadingAnimHolder}></h5>
-                <h6 className={commonClasses.PulseLoadingAnimHolder}></h6>
-            </li>
-        )
-    }
-
-    return <ul className={classes.DummyContent}>{result}</ul>;
-}
-
-function renderMessage(text: string, iamgeFileName: string, alt: string) {
-    return (
-        <div className={classes.Message}>
-            <img src={`/ilustrations/${iamgeFileName}`} alt={alt}/>
-            <h5>{text}</h5>
-        </div>
-    );
-}
 
 export default function PendingShiftsWidget() {
     const [shiftsCount, setShiftsCount] = useState<number | null>(null);
@@ -48,20 +13,27 @@ export default function PendingShiftsWidget() {
 
     useEffect(()=>{
         return callAPI<API.App.Widgets.GetPendingShiftsCount.IEndpoint>("GET","/api/widgets/pending-shifts-count",null,
-            data=>setShiftsCount(data),
+            data=>{
+                setShiftsCount(data);
+                setWidgetState("Ready");
+            },
             ()=>setWidgetState("Unavailable"));
     }, []);
 
     return (
         <div className={classes.WidgetWrapper}>
             {
-                shiftsCount!==null?
-                    shiftsCount>0?
-                        renderMainPanel()
-                    :
-                    renderMessage("All done","Celebrating.svg","Celebrations")
+                shiftsCount&&shiftsCount>0?
+                    <div className={classes.MainContent}>
+                        <h4>3 shifts</h4>
+                        <h5>You still have shifts this month that require finishing.</h5>
+                        <Link className={"button"} to={"/Statistics/Shifts?state=pending"}>Go to shifts</Link>
+                    </div>
                 :
-                    renderDummyContent()
+                    <div className={classes.Message}>
+                        <img src="/ilustrations/Celebrations.svg" alt="Celebrations"/>
+                        <h5>All done</h5>
+                    </div>
             }
             
         </div>
