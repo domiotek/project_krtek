d9e6d77d5d60108509abd7a40e752b6d4ad2ac9c
Damian <damianomiotek@outlook.com>
2023-12-11 18:37:54 +0100

    Localized 404 page

--- src/public/pages/NoPage.tsx
+++ src/public/pages/NoPage.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./NoPage.css";
+import { useTranslation } from "react-i18next";
 
 export default function NoPage(){
+    const {t} = useTranslation("not-found");
+
     return (
         <section className={styles.FlexContainer}>
             <img src="/ilustrations/ResourceNotFound.svg" alt="'404' Image" />
             <div className={styles.MessageHolder}>
-                <h3>Page not found</h3>
-                <p>Resource you are looking for doesn't exist under that address.</p>
-                <Link to="/" className="button">Take me home</Link>
+                <h3>{t("header")}</h3>
+                <p>{t("desc")}</p>
+                <Link to="/" className="button">{t("link")}</Link>
             </div>
         </section>
     );
