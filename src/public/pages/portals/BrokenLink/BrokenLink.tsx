3e93f71d65d64eeb72fbee3b171dcd974d603736
Damian <damianomiotek@outlook.com>
2023-12-11 23:18:17 +0100

    Localized all portals

--- src/public/pages/portals/BrokenLink/BrokenLink.tsx
+++ src/public/pages/portals/BrokenLink/BrokenLink.tsx
@@ -3,10 +3,13 @@ import React, { useEffect, useState } from "react";
 import PortalClasses from "../Portals.css";
 import classes from "./BrokenLink.css";
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 export default function BrokenLinkPortal() {
     const [isServerIssue, setIsServerIssue] = useState(false);
     const navigate = useNavigate();
+
+    const {t} = useTranslation("portals", {keyPrefix: "brk-link"});
 
     useEffect(()=>{
         const params = new URLSearchParams(window.location.search);
@@ -18,15 +21,15 @@ export default function BrokenLinkPortal() {
     return (
         <div className={`${PortalClasses.Portal} ${classes.BrokenLinkPortal}`}>
             <img src="/ilustrations/BrokenCar.svg" alt="Broken"/>
-            <h3>This link doesn't work</h3>
+            <h3>{t("header")}</h3>
             <p>
                 {isServerIssue?
-                    "But it's not your fault! We ran into a problem. Try again in a moment and if the issue persists, please contact us - we will be happy to help."
+                    t("desc_server")
                 :
-                    "This link is invalid or already expired."
+                    t("desc_client")
                 }
             </p>
-            <button type="button" onClick={()=>navigate("/Login")}>Go home</button>
+            <button type="button" onClick={()=>navigate("/Login")}>{t("link")}</button>
         </div>
     )
 }
